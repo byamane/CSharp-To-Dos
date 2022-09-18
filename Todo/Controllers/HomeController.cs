27a84e6706319de42d80c57a8c966a10f399b5db
byamane <byamane21@gmail.com>
2022-09-18 15:01:30 -0500

    add initial Todo form

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -2,30 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Todo.Models;
 
-namespace Todo.Controllers;
-
-public class HomeController : Controller
+namespace Todo.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
