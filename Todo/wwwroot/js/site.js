e3736e74d368a2f6e6d8cf0611f4a08a54f0f5f7
byamane <byamane21@gmail.com>
2022-09-18 18:05:04 -0500

    add JS functionality for populating form for update function

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -10,3 +10,21 @@
     }
   })
 }
+
+function populateForm(i) {
+
+  $.ajax({
+      url: 'Home/PopulateForm',
+      type: 'GET',
+      data: {
+          id: i
+      },
+      dataType: 'json',
+      success: function (response) {
+          $("#Todo_Name").val(response.name)
+          $("#Todo_Id").val(response.id)
+          $("#form-button").val("Update Todo")
+          $("#form-action").attr("action", "/Home/Update")
+      }
+  })
+}
